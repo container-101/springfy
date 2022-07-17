25425fc68524b889657c21d46acd301816aa5203
woodi97 <woodi.daily@gmail.com>
2022-07-17 20:18:12 +0900

    Init Spring Boot Boilerplate

---
+++ src/main/java/com/container101/springfy/SpringfyApplication.java
@@ -0,0 +1,13 @@
+package com.container101.springfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringfyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/container101/springfy/SpringfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.container101.springfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
